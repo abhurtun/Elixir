84cfc4e10d754705ee58a594f3b7d2a1c6189711
Arvin Bhurtun <abhurtun@dctfh.com>
2016-09-03 10:53:03 +0100

    completed exercise

--- lib/koans/09_pattern_matching.ex
+++ lib/koans/09_pattern_matching.ex
@@ -2,39 +2,39 @@ defmodule PatternMatching do
   use Koans
 
   koan "One matches one" do
-    assert match?(1, ___)
+    assert match?(1, 1)
   end
 
   koan "A value can be bound to a variable" do
     a = 1
-    assert a == ___
+    assert a == 1
   end
 
   koan "A variable can be rebound" do
     a = 1
     a = 2
-    assert a == ___
+    assert a == 2
   end
 
   koan "A variable can be pinned to prevent it from being rebounded" do
     a = 1
     assert_raise MatchError, fn() ->
-      ^a = ___
+      ^a = 2
     end
   end
 
   koan "Patterns can be used to pull things apart" do
     [head | tail] = [1, 2, 3, 4]
 
-    assert head == ___
-    assert tail == ___
+    assert head == [1]
+    assert tail == [2, 3, 4]
   end
 
   koan "And then put them back together" do
     head = 1
     tail = [2, 3, 4]
 
-    assert ___ == [head | tail]
+    assert [1,2,3,4] == [head | tail]
   end
 
   koan "Some values can be ignored" do
